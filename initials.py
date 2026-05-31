62a571fc5fbeea2bf7b85b8be129345bc8c6fb08
ghana-codes <seakomom@gmail.com>
2026-05-31 19:21:50 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Marvin Seakomo
+# STUDENT ID: 2425403287
+# Email: 2425403287@live.gctu.edu.gh
+# Grader: Marvin
+#
+# On my honor, Marvin Seakomo, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in small and large stylized letters.
 
+    print()
+
+    print("...MSM")
+
+    print()
+
+    print("...MMMMMMMMMMMM...SSSSSSSSSSSS...MMMMMMMMMMMM")
+    print("...MM........MM...SS..........S...MM........MM")
+    print("...MMM......MMM...SSS.............MMM......MMM")
+    print("...MM.M....M.MM...SSSSSSSSSSSS....MM.M....M.MM")
+    print("...MM..M..M..MM............SSS....MM..M..M..MM")
+    print("...MM...MM...MM.............SS....MM...MM...MM")
+    print("...MM........MM.............SS....MM........MM")
+    print("...MM........MM...S........SSS....MM........MM")
+    print("...MM........MM...SSSSSSSSSSSS....MM........MM")
+    print("...MMMMMMMMMMMM...SSSSSSSSSSSS....MMMMMMMMMMMM")
+
+    print()
 
 
 main()
